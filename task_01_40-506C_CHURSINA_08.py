134e0fd30b17538ffa9ffc41fd4b704d5409c33c
vasilevs_msk@mail.ru <vasilevs_msk@mail.ru>
2020-05-18 13:53:51 +0300

    second

--- task_01_40-506C_CHURSINA_08.py
+++ task_01_40-506C_CHURSINA_08.py
@@ -5,14 +5,14 @@ import os
 try: os.mkdir('results')
 except OSError: pass
 w = open('results/task_01_40-506C_CHURSINA_8.txt', 'w')
-print('x\tf(x)\n', file = w)
+print('x{0:4}f(x)\n'.format(' '), file = w)
 dx = 0.05
 x = np.arange(-10, 10 + dx, dx)
 A = 9.66459
 f = -abs(np.sin(x) * np.cos(A) * np.exp(abs(1 -
                 (x**2 + A**2) ** 0.5 / np.pi)))
 for i in range(0,len(x)):
-    print(x[i],'\t',f[i], file = w)
+    print('{0}{1:4}{2}'.format(x[i],' ',f[i]), file = w)
 w.close()
 
 plt.plot(x,f)
